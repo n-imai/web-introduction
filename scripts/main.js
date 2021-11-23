9331055776be17fa172d5bdf062a6aa4b165681f
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2021-11-23 19:17:50 +0900

    add view list

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const hero = new HeroSlider('.swiper-container');
     hero.start();
 
-    const cb = function (el, isIntersecting) {
-        if(isIntersecting) {
+    const cb = function (el, inview) {
+        if(inview) {
             const ta = new TweenTextAnimation(el);
             ta.animate();
         }
     }
 
     const so = new ScrollObserver('.tween-animate-title', cb);
+    const _inviewAnimation = (el ,inview) => {
+      if(inview){
+          el.classList.add('inview');
+      }else{
+          el.classList.add('inview')
+      }
+    }
+    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
 });
